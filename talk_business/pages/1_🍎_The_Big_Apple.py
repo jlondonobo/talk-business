87df4aa1a028ae05edd4eef558c7b2bc42962c66
jlondonobo <j.londonobo@gmail.com>
2023-04-12 00:12:31 -0500

    ✨ Add grouped count variables

--- talk_business/pages/1_🍎_The_Big_Apple.py
+++ talk_business/pages/1_🍎_The_Big_Apple.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils.columns import COLUMNS
+from utils.columns import GROUPED_COUNTS, GROUPED_STATS, STATS
 from utils.options import COUNTIES, METRICS
 from utils.plots.blocks import plot_blocks_choropleth
 
@@ -15,16 +15,20 @@ counties = st.multiselect("Select a county", COUNTIES, COUNTIES[0])
 metric = st.selectbox("Select a metric", list(METRICS.keys()), format_func=METRICS.get)
 
 if metric in ["TOTAL_POPULATION", "DENSITY_POP_SQMILE"]:
-    population = get_total_population(counties)
+    data = get_total_population(counties)
+    colname = metric
 
-    map = plot_blocks_choropleth(population, "CENSUS_BLOCK_GROUP", metric)
-    st.plotly_chart(map, use_container_width=True)
-
-else:
-    variant = st.radio("Select a variant", list(COLUMNS[metric].keys()), index=0)
+elif metric in GROUPED_STATS:
+    variant = st.radio("Select a variant", list(GROUPED_STATS[metric].keys()), index=0)
     colname = f"{metric}_{variant}"
+    data = get_simple_column(counties, GROUPED_STATS[metric][variant], colname)
 
-    data = get_simple_column(counties, COLUMNS[metric][variant], colname)
-    
-    map = plot_blocks_choropleth(data, "CENSUS_BLOCK_GROUP", colname.upper())
-    st.plotly_chart(map, use_container_width=True)
+elif metric in GROUPED_COUNTS:
+    variant = st.radio("Select a variant", list(GROUPED_COUNTS[metric].keys()), index=0)
+    agg_type = st.radio("Aggregation", ["TOTAL", "PERCENTAGE"], index=0)
+    colname = f"{metric}_{variant}_{agg_type}"    
+    data = get_simple_column(counties, GROUPED_COUNTS[metric][variant], colname, agg_type)
+
+
+map = plot_blocks_choropleth(data, "CENSUS_BLOCK_GROUP", colname.upper())
+st.plotly_chart(map, use_container_width=True)
