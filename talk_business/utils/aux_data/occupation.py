fe093639a59fbe22703ad61b0a1d7a0e9158430a
José Londoño <43001823+jlondonobo@users.noreply.github.com>
2023-04-13 23:44:25 -0500

    ✨ Add NTA code and name (#6)
    * ✚ Add `openpyxl` for migrations
    
    * 🚧 Upload data
    
    * 🐛 Fix bug in migration
    
    * 🚧 Group occupation
    
    * ✨ Add median gross rent
    
    * 📝 Add short data description

---
+++ talk_business/utils/aux_data/occupation.py
@@ -0,0 +1,70 @@
+OCCUPATION = {
+    "Building and grounds cleaning and maintenance occupations": [
+        "C24010e25",
+        "C24010e61",
+    ],
+    "Computer engineering and science occupations": [
+        "C24010e43",
+        "C24010e7",
+    ],
+    "Construction and extraction occupations": [
+        "C24010e32",
+        "C24010e68",
+    ],
+    "Education legal community service arts and media occupations": [
+        "C24010e11",
+        "C24010e47",
+    ],
+    "Farming fishing and forestry occupations": [
+        "C24010e31",
+        "C24010e67",
+    ],
+    "Food preparation and serving related occupations": [
+        "C24010e24",
+        "C24010e60",
+    ],
+    "Healthcare practitioners and technical occupations": [
+        "C24010e16",
+        "C24010e52",
+    ],
+    "Healthcare support occupations": [
+        "C24010e20",
+        "C24010e56",
+    ],
+    "Installation maintenance and repair occupations": [
+        "C24010e33",
+        "C24010e69",
+    ],
+    "Management business and financial occupations": [
+        "C24010e4",
+        "C24010e40",
+    ],
+    "Material moving occupations": [
+        "C24010e37",
+        "C24010e73",
+    ],
+    "Office and administrative support occupations": [
+        "C24010e29",
+        "C24010e65",
+    ],
+    "Personal care and service occupations": [
+        "C24010e26",
+        "C24010e62",
+    ],
+    "Production occupations": [
+        "C24010e35",
+        "C24010e71",
+    ],
+    "Protective service occupations": [
+        "C24010e21",
+        "C24010e57",
+    ],
+    "Sales and related occupations": [
+        "C24010e28",
+        "C24010e64",
+    ],
+    "Transportation occupations": [
+        "C24010e36",
+        "C24010e72",
+    ],
+}
