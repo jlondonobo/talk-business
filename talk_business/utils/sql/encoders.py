5fbf5f96ca1296326df090e9d8d5f7a88a4470a4
jlondonobo <j.londonobo@gmail.com>
2023-04-15 01:03:55 -0500

    🔥 Remove unused functions

--- talk_business/utils/sql/encoders.py
+++ talk_business/utils/sql/encoders.py
@@ -16,22 +16,3 @@ def encode_select(column: str, alias: str) -> str:
     """Econdes column and alias pair for use in a SELECT statement."""
     return f'"{column}" AS {alias}'
 
-
-def preprocess_column_group(
-    column_groups: dict[str, dict[str, dict[str, str]]]
-) -> list[dict[str, str]]:
-    """Preprocesses a column group for use in a SELECT statement."""
-    columns_sequence = []
-    for group, columns in column_groups.items():
-        for column, variations in columns.items():
-            for variation, code in variations.items():
-                columns_sequence.append(
-                    {"column": code, "alias": f"{column}-{variation}"}
-                )
-
-    return columns_sequence
-
-
-def encode_columns(column_group: dict[str, dict[str, dict[str, str]]]) -> str:
-    preprocessed_columns = preprocess_column_group(column_group)
-    return ", ".join([encode_select(**column) for column in preprocessed_columns])
