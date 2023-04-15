1474935054c1ba48521a8f984269895e36ea0ddc
jlondonobo <j.londonobo@gmail.com>
2023-04-14 19:27:40 -0500

    ♻️ Refactor

--- talk_business/utils/columns.py
+++ talk_business/utils/columns.py
@@ -1,45 +1,41 @@
-STATS = {
-    # "TOTAL_POPULATION": "B01003e1",
-    "PER_CAPITA_INCOME": "B19301e1",
-    "MEDIAN_GROSS_RENT": "B25064e1",
-    "HOUSEHOLD_SIZE": "B25010e1",
-}
-
-
-GROUPED_STATS = {
-    "MEDIAN_AGE": {
-        "TOTAL": "B01002e1",
-        "MALE": "B01002e2",
-        "FEMALE": "B01002e3",
+COLUMNS = {
+    "METRICS": {
+        "PER_CAPITA_INCOME": {
+            "METRIC": "B19301e1",
+        },
+        "MEDIAN_GROSS_RENT": {
+            "METRIC": "B25064e1",
+        },
+        "HOUSEHOLD_SIZE": {
+            "METRIC": "B25010e1",
+        },
+        "TOTAL_POPULATION": {
+            "METRIC": "B01003e1"
+        },
     },
-    # Unit: housing units
-    "OCCUPANCY_STATUS": {
-        "TOTAL": "B25002e1",
-        "OCCUPIED": "B25002e2",
-        "VACANT": "B25002e3",
+    "SEGMENTED_STATISTIC": {
+        "MEDIAN_AGE": {
+            "TOTAL": "B01002e1",
+            "MALE": "B01002e2",
+            "FEMALE": "B01002e3",
+        },
+    },
+    "SEGMENTED_COUNT": {
+        "RACE": {
+            "TOTAL": "B02001e1",
+            "WHITE": "B02001e2",
+            "BLACK_OR_AFRICAN_AMERICAN": "B02001e3",
+            "AMERICAN_INDIAN_AND_ALASKA_NATIVE": "B02001e4",
+            "ASIAN": "B02001e5",
+            "NATIVE_HAWAIIAN_AND_OTHER_PACIFIC_ISLANDER_ALONE": "B02001e6",
+            "SOME_OTHER_RACE": "B02001e7",
+            "TWO_OR_MORE_RACES": "B02001e8",
+        },
+        "OCCUPANCY_STATUS": {
+            "TOTAL": "B25002e1",
+            "OCCUPIED": "B25002e2",
+            "VACANT": "B25002e3",
+        }
     }
 }
-
-
-GROUPED_COUNTS = {
-    "RACE": {
-        "WHITE": "B02001e2",
-        "BLACK_OR_AFRICAN_AMERICAN": "B02001e3",
-        "AMERICAN_INDIAN_AND_ALASKA_NATIVE": "B02001e4",
-        "ASIAN": "B02001e5",
-        "NATIVE_HAWAIIAN_AND_OTHER_PACIFIC_ISLANDER_ALONE": "B02001e6",
-        "SOME_OTHER_RACE": "B02001e7",
-        "TWO_OR_MORE_RACES": "B02001e8",
-    }
-}
-
-
-
-
-
-
-
-
-
-
 
