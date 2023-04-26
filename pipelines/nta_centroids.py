818a71e8b3b40b22f5b53d88d0c48f0f4b3d41bd
jlondonobo <j.londonobo@gmail.com>
2023-04-26 01:17:35 -0500

    ♻️ Update nta centroids data

--- pipelines/nta_centroids.py
+++ pipelines/nta_centroids.py
@@ -5,16 +5,16 @@ from utils.pandas_exporter import init_connection, run_query
 def fetch_nta_centroids(conn):
     query = """
     WITH name_mapper AS (
-        SELECT DISTINCT NTA_NAME, NTA_CODE
+        SELECT DISTINCT NTA_NAME, NTA_CODE, COUNTY_FIPS
         FROM PERSONAL.PUBLIC.NTA_MAPPER
     ),
     centroids AS (
-        SELECT NTA_CODE, ST_CENTROID(to_geography(geometry)) as geom, NTA_NAME
+        SELECT COUNTY_FIPS, NTA_CODE, ST_CENTROID(to_geography(geometry)) as geom, NTA_NAME
         FROM PERSONAL.PUBLIC.NTA_GEOGRAPHY
         LEFT JOIN name_mapper USING(NTA_CODE)
     )
 
-    SELECT NTA_CODE, NTA_NAME, ST_Y(geom) AS LAT, ST_X(geom) AS LON
+    SELECT COUNTY_FIPS, NTA_CODE, NTA_NAME, ST_Y(geom) AS LAT, ST_X(geom) AS LON
     FROM centroids;
     """
     return run_query(conn, query)
