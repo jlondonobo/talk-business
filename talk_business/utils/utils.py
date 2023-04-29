fc81bc6ec5d8e83ded68bd6021f0b27b026ce8ee
jlondonobo <j.londonobo@gmail.com>
2023-04-28 23:41:27 -0500

    ✨ Add stylesheet loader

--- talk_business/utils/utils.py
+++ talk_business/utils/utils.py
@@ -7,3 +7,9 @@ def app_base_config():
         initial_sidebar_state="expanded",
         layout="wide",
     )
+
+
+def load_css(file_name: str) -> None:
+    """Import a CSS file into the Streamlit app."""
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
